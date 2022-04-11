8c5f33d483636ebf8ecd98e44c54246f2da84e34
yeinerrestrepo01 <yeinermeri@gmail.com>
2022-04-11 16:01:23 -0500

    Documentacion swgger y configuraicon de proyecto acceso a datos

--- XOLIT.Productos.API/Startup.cs
+++ XOLIT.Productos.API/Startup.cs
@@ -1,12 +1,81 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using XOLIT.Productos.Infrastructure.DBContext;
 
 namespace XOLIT.Productos.API
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public Startup(IConfiguration configuration)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region Swagger
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(swagger =>
+            {
+                var contact = new OpenApiContact
+                {
+                    Name = "XOLIT.Productos.API"
+                };
+
+                swagger.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = "XOLIT Productos API",
+                    Version = "v1",
+                    Description = "XOLIT Productos API - Documentation Services",
+                    Contact = contact
+                });
+
+
+            });
+
+            #endregion Swagger
+
+            string connection = Configuration.GetConnectionString("CaseLinkDbContext");
+            services.AddDbContext<XolitDbContext>(options => options.UseSqlServer(connection).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+
+            #region Register (dependency injection)
+            //IoC.AddDependency(services);
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CaseLink.Core.WebApi v1"));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseMiddleware<RequestResponseMiddleware>();
+
+            app.ConfigureExceptionHandler(logger);
+            app.UseAuthentication();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
