681a00bb3f580d7f57d7d203b4815e1206403b5f
yeinerrestrepo01 <yeinermeri@gmail.com>
2022-04-11 16:30:36 -0500

    ajuste de configuracion e implementacion de swagger

--- XOLIT.Productos.API/Startup.cs
+++ XOLIT.Productos.API/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using XOLIT.Productos.Infrastructure.DBContext;
@@ -6,50 +6,86 @@ using XOLIT.Productos.Infrastructure.DBContext;
 namespace XOLIT.Productos.API
 {
     public class Startup
+
     {
+
         public Startup(IConfiguration configuration)
+
         {
+
             Configuration = configuration;
+
         }
+
+
 
         public IConfiguration Configuration { get; }
 
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
+
         {
+
+            string connection = Configuration.GetConnectionString("XOLOTDbContext");
+
+            services.AddDbContext<XolitDbContext>(options => options.UseSqlServer(connection).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+
+
+
             #region Swagger
 
+
+
             // Register the Swagger generator, defining one or more Swagger documents
+
             services.AddSwaggerGen(swagger =>
             {
                 var contact = new OpenApiContact
                 {
-                    Name = "XOLIT.Productos.API"
+                    Name = "XOLIT productos API"
+
                 };
-
                 swagger.SwaggerDoc("v1",
+
                 new OpenApiInfo
+
                 {
-                    Title = "XOLIT Productos API",
+
+                    Title = "XOLIT.Productos.API",
+
                     Version = "v1",
-                    Description = "XOLIT Productos API - Documentation Services",
+
+                    Description = "XOLIT Productos  API - Documentation Services",
+
                     Contact = contact
+
                 });
-
-
             });
-
-            #endregion Swagger
-
-            string connection = Configuration.GetConnectionString("CaseLinkDbContext");
-            services.AddDbContext<XolitDbContext>(options => options.UseSqlServer(connection).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
-
-            #region Register (dependency injection)
-            //IoC.AddDependency(services);
+            services.AddControllers();
             #endregion
+
+
+
+            //APPINSIGHTS
+
+            ApplicationInsightsServiceOptions aiOptions = new ApplicationInsightsServiceOptions
+
+            {
+
+                InstrumentationKey = Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY")
+
+            };
+
+            services.AddApplicationInsightsTelemetry(aiOptions);
         }
 
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
@@ -58,11 +94,14 @@ namespace XOLIT.Productos.API
             }
 
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CaseLink.Core.WebApi v1"));
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Panel.BI.Integraciones.API v1"));
+
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
             app.UseAuthentication();
 
             app.UseAuthentication();
@@ -70,9 +109,15 @@ namespace XOLIT.Productos.API
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
+
             {
+
                 endpoints.MapControllers();
+
             });
+
         }
+
     }
+
 }
