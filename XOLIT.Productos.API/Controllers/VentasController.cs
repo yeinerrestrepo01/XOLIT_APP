94423f812acf84b5db2a4f558255b403b1257136
yeinerrestrepo01 <yeinermeri@gmail.com>
2022-04-11 16:33:49 -0500

    creacion controlador de ventas

---
+++ XOLIT.Productos.API/Controllers/VentasController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace XOLIT.Productos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VentasController : ControllerBase
+    {
+        // GET: api/<VentasController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<VentasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<VentasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<VentasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<VentasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
